a4f2fe578387ba2c052f8af47c414600a3c953e0
giapdq04 <quanggiap04@gmail.com>
2025-03-05 14:54:56 +0700

    thêm dotenv

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb+srv://giapdqph34273:sf7pdIYAvFqwDlje@testmongodb.3f1oxwh.mongodb.net/TheKey_dev');
+        await mongoose.connect(process.env.DATABASE);
         console.log('Connect successfully');
     } catch (error) {
         console.log('Connect fail');
